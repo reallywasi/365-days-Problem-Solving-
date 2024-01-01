4ee116325df32aa9d3ff5ced89bd56eb9efe4ff9
M Wasi <reallywasi@gmail.com>
2024-01-01 22:41:23 +0530

    Update remove_duplicates.cpp

--- remove_duplicates.cpp
+++ remove_duplicates.cpp
@@ -18,3 +18,32 @@ public:
        
     }
 };
+
+
+
+
+
+//EXPLANATION 
+
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();  // Get the size of the input vector
+        int j = 1;  // Initialize the position for the modified array (without duplicates)
+
+        for (int i = 1; i < n; i++) {
+            // Check if the current element is different from the previous one
+            if (nums[i] != nums[i - 1]) {
+                // If different, update the modified array at position 'j'
+                // and increment 'j' to point to the next position
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        // 'j' now represents the size of the modified array without duplicates
+        return j;
+    }
+};
+
